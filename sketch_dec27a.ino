1767cbf320e4c6d8f2a20bdb1b578587965d6906
hwangchandong <116860737+hwangchandong@users.noreply.github.com>
2023-12-27 10:24:00 +0900

    Add files via upload

---
+++ sketch_dec27a.ino
@@ -0,0 +1,59 @@
+// 바운드 헤더파일 시리얼 출력
+
+#include <Bounce2.h>
+
+int button = 2;
+int button1 = 3;
+
+int led = 12;
+int led1 = 13;
+
+Bounce debouncer = Bounce();
+Bounce debouncer1 = Bounce();
+
+
+void setup() {
+
+  pinMode(button,INPUT_PULLUP);
+  debouncer.attach(button);
+  debouncer.interval(5);
+  pinMode(button1,INPUT_PULLUP);
+  debouncer1.attach(button1);
+  debouncer1.interval(5);
+  
+  Serial.begin(9600);
+  
+ 
+  
+}
+
+void loop() {
+  debouncer.update();
+  debouncer1.update();
+
+  
+  if(debouncer.fell())
+  {
+    
+    Serial.println("right");
+    digitalWrite(led, HIGH); 
+   
+  }
+  if(debouncer.rose())
+  {
+    //Serial.println(" ");
+    digitalWrite(led, LOW); 
+  }  
+
+  
+  if(debouncer1.fell())
+  {
+    Serial.println("left");
+    digitalWrite(led1, HIGH);
+  }
+  if(debouncer1.rose())
+  {
+    //Serial.println(" ");
+    digitalWrite(led1, LOW);
+  }  
+}
